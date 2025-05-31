27511ebd9bd9a774afbb20c41b7eab4aa26fd4fb
Rubem Neto <rubemviscard2635@gmail.com>
2025-05-31 10:51:17 +0100

    feat:content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,md,mdx}",
   ],
   theme: {
     extend: {},
