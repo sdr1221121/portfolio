18a13ef1f3e566939ed15fc9ed91d7ad8d330eca
sdr1221121 <1221121@isep.ipp.pt>
2025-08-05 15:37:09 +0100

    astro file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/serverless'; // usa a versão serverless para Vercel
 
-import vercel from '@astrojs/vercel';
-
-// https://astro.build/config
 export default defineConfig({
+  output: 'server', // necessário para Vercel funcionar correctamente
   vite: {
     plugins: [tailwindcss()]
   },
-
   adapter: vercel({
     webAnalytics: {
       enabled: true,
